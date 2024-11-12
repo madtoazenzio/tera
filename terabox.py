0d2c2401b633afc363f8713a883bf24f857ea6de
メ™Asʏɴ Eᴅɪᴛᴢ​ゞメ <89691212+madtoazenzio@users.noreply.github.com>
2024-11-12 06:53:21 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -56,7 +56,7 @@ async def start_command(client, message):
     join_button = InlineKeyboardButton("ᴊᴏɪɴ ❤️🚀", url="https://t.me/zooistore")
     developer_button = InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ⚡️", url="https://t.me/prndevika_bot")
     reply_markup = InlineKeyboardMarkup([[join_button, developer_button]])
-    video_file_id = "/app/Jet-Mirror.mp4"
+    video_file_id = "/app/IMG_20241112_065101_717.jpg"
     if os.path.exists(video_file_id):
         await client.send_video(
             chat_id=message.chat.id,
